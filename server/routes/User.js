33c67bbbe44c9606bd8cdbf14ca888e1ca1dca01
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-11 17:46:11 -0500

    userRoute

---
+++ server/routes/User.js
@@ -0,0 +1,32 @@
+const User = require("../models/userinfo");
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+
+router.post('/', async(req, res)=> {
+    try {
+        const {email, name, password} = req.body;
+        const user = await User.create({email, name, password});
+        res.status(300).json(user);
+    } catch (e) {
+        res.status(400).json({
+          message: "An error occurred",
+          error: error.message,
+        })
+    }
+        
+}) 
+
+
+
+router.post('/login', async(req, res)=> {
+    try {
+        const {email, name, password} = req.body;
+        const user = await User.findByCredentials({email, name, password});
+        user.status = 'online';
+        await user.save();
+        res.status(200).json(user);
+    } catch (e) {
+          res.status(400).json(e.message)
+      }
+        
+}) 
