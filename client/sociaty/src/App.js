24db0e562d3d84f962ec3acc515f4ddda4aa708e
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-11 01:53:27 -0500

    navbar

--- client/sociaty/src/App.js
+++ client/sociaty/src/App.js
@@ -1,21 +1,25 @@
 import Navigation from "./components/nav";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from "./pages/homepage";
+import Signup from "./pages/signup";
+import Login from "./pages/login";
+import Chat from "./pages/chatpage";
+
 
 function App() {
   return (
     <BrowserRouter>
-        <Navigation />
-            <Routes>
-              <Route path="/" element={<Home />} />
-                    
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-                       
-                   
-              <Route path="/chat" element={<Chat />} />
-            </Routes>
-      </BrowserRouter>
+      <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/chat" element={<Chat />} />
+        </Routes>
+    </BrowserRouter>
   );
 }
+
+
 
 export default App;
