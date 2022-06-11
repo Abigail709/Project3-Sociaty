1790ffb661948fb898e93e32e2fcfa8284a90e32
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-11 01:14:17 -0500

    routes

--- client/sociaty/src/App.js
+++ client/sociaty/src/App.js
@@ -1,10 +1,20 @@
 import Navigation from "./components/nav";
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Navigation />
-    </div>
+    <BrowserRouter>
+        <Navigation />
+            <Routes>
+              <Route path="/" element={<Home />} />
+                    
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+                       
+                   
+              <Route path="/chat" element={<Chat />} />
+            </Routes>
+      </BrowserRouter>
   );
 }
 
