6c7057e8a400944e6e507ac7fecc991a8c7f832d
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-11 15:27:16 -0500

    chatpage

--- client/sociaty/src/pages/chatpage.js
+++ client/sociaty/src/pages/chatpage.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button';
+import {Container, Col, Row} from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+import {Link} from 'react-router-dom';
+import Sidebar from '../components/sidebar';
+import Message from '../components/message';
 
 function Chat() {
     return (
-      <div>Chat</div>
+      <Container>
+         <Row>
+          <Col>
+            <Sidebar />
+          </Col>
+
+          <Col>
+            <Message />
+          </Col>
+          </Row>
+          
+      </Container>
     );
   }
   
--- client/sociaty/src/pages/signup.js
+++ client/sociaty/src/pages/signup.js
@@ -5,7 +5,7 @@ import Form from 'react-bootstrap/Form';
 import {Link} from 'react-router-dom';
 
 
-async function Signup() {
+function Signup() {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -15,28 +15,7 @@ async function Signup() {
     const [submitted, setSubmitted] = useState(false);
 
 
-    const handleName = (e) => {
-       
-
-        setName(e.target.value);
-        
-    };
-
-    //Handling the email change
-    const handleEmail = (e) => {
-        
-
-        setEmail(e.target.value);
-        
-    };
-
-    // Handling the password change
-    const handlePassword = (e) => {
-        
-
-        setPassword(e.target.value);
-       
-    };
+    
 
     const handleSignup = (e) => {
         e.preventDefault();
@@ -53,14 +32,14 @@ async function Signup() {
                 <Form onSubmit={handleSignup}>
                 <Form.Group className="mb-3" controlId="formBasicEmail" >
                   <Form.Label>Name</Form.Label>
-                  <Form.Control type="name" placeholder="Enter name" onChange={handleName} />
+                  <Form.Control type="name" placeholder="Enter name"  />
                   
                 </Form.Group>
         
         
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Email address or Phone Number</Form.Label>
-                  <Form.Control type="email" placeholder="Enter email / Phone Number" onChange={handleEmail} />
+                  <Form.Control type="email" placeholder="Enter email / Phone Number"  />
                   <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                   </Form.Text>
@@ -68,7 +47,7 @@ async function Signup() {
           
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                   <Form.Label>Password</Form.Label>
-                  <Form.Control type="password" placeholder="Password" onChange={handlePassword}/>
+                  <Form.Control type="password" placeholder="Password" />
                 </Form.Group>
                 {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
                   <Form.Check type="checkbox" label="Check me out" />
