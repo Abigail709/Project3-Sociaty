e21bca68fbcbf335a6f0997e1e1e0523caed1a47
Abigail709 <101914320+Abigail709@users.noreply.github.com>
2022-06-12 04:12:43 -0500

    adding function signup

--- client/sociaty/src/pages/signup.js
+++ client/sociaty/src/pages/signup.js
@@ -3,10 +3,11 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/esm/Container';
 import Form from 'react-bootstrap/Form';
 import {Link} from 'react-router-dom';
+import axios from "axios";
 
 
 function Signup(value, setValue) {
-    // const [name, setName] = useState('');
+    // const [username, setName] = useState('');
     // const [email, setEmail] = useState('');
     // const [password, setPassword] = useState('');
 
@@ -22,8 +23,11 @@ function Signup(value, setValue) {
 
     
 
-    const handleSignup = (e) => {
+    const handleSignup = async (e) => {
         e.preventDefault();
+        if (handleValidation()) {
+          
+        }
 
           
     }
@@ -33,18 +37,26 @@ function Signup(value, setValue) {
         
     // }
 
+    
+    
+
     const handleChange = (event) => {
       setValues({ ...values, [event.target.name]: event.target.value });
     };
                
-
+    const handleValidate =() => {
+      const {username, email, password, confirmPassword} = values;
+      if (password !== confirmPassword) {
+       alert('Enter matching password')
+      }
+    };
 
     return (
                 <Container>
                 <Form onSubmit={handleSignup}>
                 <Form.Group className="mb-3" controlId="formBasicEmail" >
-                  <Form.Label>Name</Form.Label>
-                  <Form.Control type="name" placeholder="Enter name" name="name" onChange={(e) => handleChange(e)} />
+                  <Form.Label>UseName</Form.Label>
+                  <Form.Control type="name" placeholder="Enter Username" name="username" onChange={(e) => handleChange(e)} />
                   
                 </Form.Group>
         
@@ -61,6 +73,11 @@ function Signup(value, setValue) {
                   <Form.Label>Password</Form.Label>
                   <Form.Control type="password" placeholder="Password" name="password" onChange={(e) => handleChange(e)} />
                 </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Confirm Password</Form.Label>
+                  <Form.Control type="password" placeholder="Confrim Password" name="confirmPassword" onChange={(e) => handleChange(e)} />
+                </Form.Group>
                 {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
                   <Form.Check type="checkbox" label="Check me out" />
                 </Form.Group> */}
@@ -74,7 +91,9 @@ function Signup(value, setValue) {
                     </p>
               </div>
               </Container>
-        
+              
+            
+              
             );
     
 }
